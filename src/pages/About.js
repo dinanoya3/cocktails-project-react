930a9a5d68d42f19fdef1304038f7724f73425a6
Dina Noya <dinanoya3@gmail.com>
2021-12-07 14:44:13 -0500

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque eius
+        natus recusandae omnis, rerum est quibusdam fugiat, blanditiis
+        consequuntur ut, officiis maiores esse expedita dolorem? Nesciunt optio
+        perferendis enim iste.
+      </p>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/pages/Error.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Error = () => {
+  return (
+    <section className="section error-page">
+      <div className="error-container">
+        <h1>Oops! It's a dead end</h1>
+        <Link to="/">
+          <button className="btn btn-primary">Back to Home</button>
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Error;
